212ec3a8812321093614fa6be2a2fc35cca20635
MadChipmunk03 <petaslachta@gmail.com>
2023-06-23 21:44:10 +0200

    Přidejte soubory projektu.

---
+++ Services/IMessengerService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalMessengerLibrary.Services
+{
+    internal interface IMessengerService<T, U>
+    {
+        T Sender { get; set; }
+        List<T> Recipients { get; set; }
+
+        bool IsValid(T recipient);
+        void AddRecipient(T recipient);
+        void FillRecipients();
+        void SendMessages(U message);
+    }
+}
---
+++ Services/TwilioBase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace UniversalMessengerLibrary.Services
+{
+    internal abstract class TwilioBase : IMessengerService<string, string>
+    {
+        public string Sender { get; set; }
+        public List<string> Recipients { get; set; } = new List<string>();
+
+        private string AccountSid { get; set; }
+        private string AuthToken { get; set; }
+
+        public TwilioBase(string accountSid, string authToken, string senderPhoneNumber)
+        {
+            AccountSid = accountSid;
+            AuthToken = authToken;
+
+            if (IsValid(senderPhoneNumber)) Sender = senderPhoneNumber;
+            else throw new Exception("Invalid phone number");
+        }
+
+        public TwilioBase(string senderPhoneNumber)
+        {
+            AccountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+            AuthToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+            if (IsValid(senderPhoneNumber)) Sender = senderPhoneNumber;
+            else throw new Exception("Invalid phone number");
+        }
+
+        public bool IsValid(string recipient)
+        {
+            return Regex.IsMatch(recipient, @"^\+\d{3}(\d{3}){3}$");
+        }
+
+        public virtual void AddRecipient(string recipient)
+        {
+            if (!IsValid(recipient)) throw new Exception("Invalid phone number");
+            Recipients.Add(recipient);
+        }
+
+        public abstract void FillRecipients();
+
+        public void SendMessages(string message)
+        {
+            TwilioClient.Init(AccountSid, AuthToken);
+
+            foreach (string recipient in Recipients)
+            {
+                // Find your Account SID and Auth Token at twilio.com/console
+                // and set the environment variables. See http://twil.io/secure
+
+                MessageResource.Create(
+                    body: message,
+                    from: new Twilio.Types.PhoneNumber(Sender),
+                    to: new Twilio.Types.PhoneNumber(recipient)
+                );
+            }
+        }
+    }
+}
